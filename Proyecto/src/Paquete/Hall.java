a202f9d9fb2ed8709c546c6eb0562707d5d0995b
RosaHuuuuuxq <rosaxiuqionghu@gmail.com>
2025-02-18 12:15:25 +0100

    Rama Halll

--- Proyecto/src/Paquete/Hall.java
+++ Proyecto/src/Paquete/Hall.java
@@ -10,6 +10,16 @@ public class Hall extends estancias {
 	
 	protected boolean blindada;
 
+	public boolean isBlindada() {
+		return blindada;
+	}
+
+
+	public void setBlindada(boolean blindada) {
+		this.blindada = blindada;
+	}
+
+
 	public Hall(String nombre, int metros2, int puertas, int ventanas, boolean blindada) {
 		super(nombre, metros2, puertas, ventanas);
 		this.blindada = blindada;
