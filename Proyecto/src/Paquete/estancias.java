e7b8d6ee697731e06ae070aeec351af442c62193
Alejandro <alexrabosomartos@gmail.com>
2025-02-18 11:51:05 +0100

    rama esqueleto

--- Proyecto/src/Paquete/estancias.java
+++ Proyecto/src/Paquete/estancias.java
@@ -13,4 +13,17 @@ public abstract class estancias {
 	//salon (R)
 	//habitacion (A)
 	//baño (A)
+	protected String nombre;
+	protected int metros2;
+	protected int puertas;
+	protected int ventanas;
+	
+	public estancias(String nombre, int metros2, int puertas, int ventanas) {
+		this.nombre = nombre;
+		this.metros2 = metros2;
+		this.puertas = puertas;
+		this.ventanas = ventanas;
+	}
+	
+	
 }
